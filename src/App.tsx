b489fffa64505d20b21cb308be55092bc49e3c5e
Mylavarapu Sarath Sai <msarathsai@proton.me>
2025-03-18 15:22:12 +0530

    sonner toast added to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <div className="min-h-screen">
       <Navbar />
       <Outlet />
+<Toaster />
     </div>
   );
 }
