5e96464bdfa2935546582731093ce3f03d99071b
Mylavarapu Sarath Sai <msarathsai@proton.me>
2025-03-20 13:19:17 +0530

    signup and login pages layout shifting when images changing, bug fixed

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -5,8 +5,8 @@ import { Outlet } from "react-router-dom";
 
 export function AuthPage() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8 min-h-[calc(100vh-3.5rem)] sm:min-h-[calc(100vh-4rem)]">
-      <div className="flex flex-col justify-center items-center p-4 sm:p-6 md:p-8 lg:p-12">
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8 min-h-[calc(100vh-3.5rem)] sm:min-h-[calc(100vh-4rem)] w-full">
+      <div className="flex flex-col justify-center items-center p-4 sm:p-6 md:p-8 lg:p-12 md:min-w-1/2">
         <div className="w-full max-w-4xl lg:max-w-5xl space-y-6 sm:space-y-8 self-center rounded-md p-4 sm:p-6 md:p-8">
           <Suspense
             fallback={
