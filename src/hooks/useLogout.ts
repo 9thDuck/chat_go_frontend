038f27aa5ecb365bf833c6dad5b6a1e80b5f06b1
Mylavarapu Sarath Sai <msarathsai@proton.me>
2025-03-21 15:54:34 +0530

    removing user from store if logout res status is 401

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,8 +1,16 @@
 import { api } from "@/lib/api";
+import { useAuthStore } from "@/store/useAuthStore";
 import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 
 export function useLogout() {
+  const { removeAuthUser } = useAuthStore();
   return useMutation({
     mutationFn: () => api.delete("/auth/logout"),
+    onError: (error: AxiosError) => {
+      if (error.response?.status === 401) {
+        removeAuthUser();
+      }
+    },
   });
 }
